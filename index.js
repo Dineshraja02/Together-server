d10f7b92098792e2dd3e5d9366129a64dff34101
Dinesh Raja <dineshraja0403@gmail.com>
2021-12-07 13:02:45 +0530

    fullfilled

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ require("dotenv").config();
 
         app.use("/api/users",userRoute);
         app.use("/api/auth",authRoute);
-        app.use("/api/post",postRoute);
+        app.use("/api/posts",postRoute);
 
         const port =process.env.PORT||3001
         app.listen(port,()=>
